2db8eea18ec966d21c6c6f855e383df5dc83c9ca
Aditya Kumar <adityak007mr@gmail.com>
2018-05-06 02:41:19 +0530

    api fix

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
+var express = require('express');
+var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://hackacthon:hackathon@ds115740.mlab.com:15740/hack';
 
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-	if (err) throw err;
-	var dbo = db.db('hack');
-	//console.log(db);
-	 dbo.collection("workers").find({}).toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
-   // db.close();
-  });
-  console.log("Connected successfully to server");
+//Requiring routes
+var location = require("./routes/location");
 
-  db.close();
+app.use("/",location);
+app.listen(process.env.PORT||3000,function(){
+  console.log("Server Started :)!");
 });
